667d759e83885d770a6b38dd934869eda351e8c9
Kay Steinhoff <kay.steinhoff@web.de>
2023-07-10 14:27:13 +0200

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,7 +1,9 @@
 function openNav() {
   document.getElementById("MainSidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
   document.getElementById("MainSidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
