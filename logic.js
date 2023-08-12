e4b51c152930a9ed71a0c0567596a20830cee372
Kay Steinhoff <kay.steinhoff@web.de>
2023-08-12 11:59:18 +0200

    Delete logic.js

--- logic.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-  document.getElementById("MainSidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-  document.getElementById("MainSidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "50px";
-}
