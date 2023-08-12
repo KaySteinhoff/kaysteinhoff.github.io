7ff375639fda240a0e5a315e2f9b908b027868b2
Kay Steinhoff <kay.steinhoff@web.de>
2023-08-12 12:00:08 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("sidenavContent").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("sidenavContent").style.width = "0";
+  document.getElementById("main").style.marginLeft = "50px";
+}
