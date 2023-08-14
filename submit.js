a3949f94fca09315763c054755e86f51b3cabe3b
KaySteinhoff <kay.steinhoff@web.de>
2023-08-14 14:30:18 +0200

    Added submition

--- submit.js
+++ submit.js
@@ -1,7 +1,1 @@
 var submitted=false;
-
-function sendEmail()
-{
-	document.getElementById("gform").fadeOut(2000);
-	document.getElementById("gform").prepend('Your submission has been processed...');
-}
