12f3236619e7864652c7e1935d4834ca1c2438c4
kaysteinhoff <kay.steinhoff@web.de>
2023-08-14 18:17:05 +0200

    Fixed sidebar side-in

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 function openNav() {
   document.getElementById("sidenavContent").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
   document.getElementById("sidenavContent").style.width = "0";
-  document.getElementById("main").style.marginLeft = "50px";
 }
 
 function sendEmail()
 {
 	document.getElementById("gform").remove();
-	document.getElementById("center").appendChild(document.createTextNode("Test"));
+	
+	var text = document.createElement("p");
+	text.appendChild(document.createTextNode("Thanks for applying to Marauder's Shadows!"));
+	text.classList.add("text");
+	text.style.marginLeft="-50px";
+	
+	document.getElementById("center").appendChild(text);
 }
