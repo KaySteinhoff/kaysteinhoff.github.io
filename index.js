b7c68a131934989d59ee2e9411573a3f4490857a
kaysteinhoff <kay.steinhoff@web.de>
2023-08-14 17:54:08 +0200

    Fixed background image

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function closeNav() {
   document.getElementById("sidenavContent").style.width = "0";
   document.getElementById("main").style.marginLeft = "50px";
 }
+
+function sendEmail()
+{
+	document.getElementById("gform").remove();
+	document.getElementById("center").appendChild(document.createTextNode("Test"));
+}
