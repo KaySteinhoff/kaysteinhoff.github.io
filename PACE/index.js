0e7053b7c647122ed23686d8077b57aded5976b8
KaySteinhoff <kay.steinhoff@as-dialoggroup.de>
2025-04-09 13:16:32 +0200

    Updated UI and added a Trivia page to hold how the syntax highlighting for the website was done

--- PACE/index.js
+++ PACE/index.js
@@ -1,3 +1,105 @@
+const cRegex = [
+	{
+		regex: /"(.*?)"/g, //strings
+		val: '#FFD658',
+		code: 'A'
+	},
+	{
+		regex: /(\d+\.?)/g, // numbers
+		val: ' #A7C',
+		code: 'B'
+	},
+	{
+		regex: /\b(char|short|int|long|float|double|unsigned|void|const|static|typedef|struct|[^\s]+_t)\b/g, // keywords and types
+		val: '#78dce8',
+		code: 'C'
+	},
+	{
+		regex: /(\w[A-Za-z0-9_]*)(?=\()/g, // functions
+		val: '#a6e22e',
+		code: 'D'
+	},
+	{
+		regex: /\b(sizeof|delete|switch|case|if|else|extern|return|for|while|break|continue|include)\b/g, // command
+		val: '#E68',
+		code: 'E'
+	},
+	{
+		regex: /\b(true|false|NULL|\$)(?=[^\w])/g, // constants
+		val: '#ae81ff',
+		code: 'F'
+	},
+	{
+		regex: /([\b\s])([!=]=|[!=]==|\+\+|--|&&|\|\|<=|>=|>>|<<|\.\.\.)(?!span)([\b\s\w])/g, // operators
+		val: '#f92672',
+		code: 'G'
+	},
+	{
+		regex: /(\/\/.*)/g, // single line comment
+		val: 'gray',
+		code: 'H'
+	},
+];
+
+const jsRegex = [
+   {
+       regex: /(\d+\.?)/g, //number
+       val: " #A7C",
+       code: "A"
+   },
+   {
+       regex: /\b(var|let|const|function|this|do|super|as|constructor|instanceof|default)\b/g, //keyword
+       val: "#78dce8",
+       code: "D"
+   },
+   {
+       regex: /\b(typeof|try|catch|finally|delete|switch|case|in|of|if|else|import|from|as|export|extends|new|return|throw|for|while|break|continue|async|await)\b/g, //command
+       val: "#E68",
+       code: "E"
+   },
+   {
+       regex: /\b(true|false|null|undefined|NaN|Infinity|\$)(?=[^\w])/g, //literal
+       val: "#ae81ff",
+       code: "F"
+   },
+   {
+       regex: /([\b\s])([!=]=|[!=]==|\+\+|--|&&|\|\||!|<=|>=|>>|<<|\.\.\.)(?!span)([\b\s\w])/g, //operator
+       val: "#f92672",
+       code: "G"
+   },
+   {
+       regex: /\b(window|document|navigator|console|self|top|process|require|module|define|global|Promise|Array|Math|String|Number|Symbol|Function|Reflect|Proxy|Error)\b/g, //global
+       val: "#fd971f",
+       code: "H"
+   },
+   {
+       regex: /(\w[A-Za-z0-9]*)(?=\()/g, //functions
+       val: "#a6e22e",
+       code: "I"
+   },
+   {
+       regex: /\b(getElementsBy(TagName|ClassName|Name)|getElementById|(get|set|remove)Attribute|querySelector(|All))(?=[^\w])/g, //DOM
+       val: "#7E7",
+       code: "J"
+   },
+      {
+       regex: /'(.*?)'/g, //st2
+       val: "#FFD658",
+       code: "B"
+   },
+   {
+       regex: /(\/\/.*)/g, //sj comment
+       val: "lightgrey",
+       code: "C"
+   },
+
+];
+
+function toggleNavBar()
+{
+	// To be implemented
+}
+
 function loadDocMd(url)
 {
 	var xmlHttp = new XMLHttpRequest();
@@ -21,6 +123,6 @@ function loadDocMd(url)
 		let CSS = [];
 		let backgroundSize = "";
 		codes[i].innerText = codes[i].innerText.replaceAll('\t', '    ');
-		codes[i].style = "background: " + convertToCSS(findTokens(codes[i].innerText)) + "; background-clip: text; -webkit-background-clip: text; background-repeat: no-repeat; color: transparent; width: 80ch; font-family: monospace; font-size: 20px; line-height: 22px;";
+		codes[i].style = "background: " + convertToCSS(findTokens(codes[i].innerText, cRegex), cRegex) + "; background-clip: text; -webkit-background-clip: text; background-repeat: no-repeat; color: transparent; font-family: monospace; ";
 	}
 }
--- PACE/syntaxHighlight.js
+++ PACE/syntaxHighlight.js
@@ -1,47 +1,3 @@
-const replacers = [
-       {
-           regex: /"(.*?)"/g, //st2
-           val: "#FFD658",
-           code: "A"
-       },
-       {
-           regex: /(\d+\.?)/g, //number
-           val: " #A7C",
-           code: "B"
-       },
-	   {
-		   regex: /(\/\/.*)/g,
-		   val: "gray",
-		   code: "C"
-	   },
-       {
-           regex: /\b(char|short|int|long|float|double|unsigned|void|const|static|typedef|struct|[^\s]+_t)\b/g, //keyword
-           val: "#78dce8",
-           code: "D"
-       },
-       {
-           regex: /(\w[A-Za-z0-9]*)(?=\()/g, //functions
-           val: "#a6e22e",
-           code: "E"
-       },
-       {
-           regex: /\b(sizeof|delete|switch|case|if|else|extern|return|for|while|break|continue|include)\b/g, //command
-           val: "#E68",
-           code: "F"
-       },
-       {
-           regex: /\b(true|false|NULL|\$)(?=[^\w])/g, //literal
-           val: "#ae81ff",
-           code: "G"
-       },
-       {
-           regex: /([\b\s\[\{\(])([!=]=|[!=]==|\+\+|--|&&|\|\||!|<=|>=|>>|<<|\.\.\.)(?!span)([\b\s\w])/g, //operator
-           val: "#f92672",
-           code: "H"
-       },
-    ];
-
-
 String.prototype.replaceAt = function(index, length, replacement) {
     if (index >= this.length) {
         return this.valueOf();
@@ -51,16 +7,15 @@ String.prototype.replaceAt = function(index, length, replacement) {
 }
 
 
-function findTokens(line){
-    
+function findTokens(line, regexObjs){
     var rtrnText = '0'.repeat(line.length);
     
-    for(x = 0; x < replacers.length; x++){
+    for(x = 0; x < regexObjs.length; x++){
         
-        let matches = [...line.matchAll(replacers[x].regex)];
+        let matches = [...line.matchAll(regexObjs[x].regex)];
         
         matches.forEach((match) => {
-            rtrnText = rtrnText.replaceAt(match.index-(match.index-line.slice(0, match.index).replaceAll('\n', '').length), match[0].length, replacers[x].code);
+            rtrnText = rtrnText.replaceAt(match.index-(match.index-line.slice(0, match.index).replaceAll('\n', '').length), match[0].length, regexObjs[x].code);
         });
     }
     return rtrnText;
@@ -68,7 +23,7 @@ function findTokens(line){
 
 
 
-function convertToCSS(line){
+function convertToCSS(line, regexObjs){
     let rtrnString = "linear-gradient(to right, ";
     let previousChar = "";
     let previousColour = "white";
@@ -83,12 +38,12 @@ function convertToCSS(line){
                 previousColour = "white";
             }else{
             
-          for(a = 0; a < replacers.length; a++){
+          for(a = 0; a < regexObjs.length; a++){
             
-              if(line[z] == replacers[a].code){
-                  rtrnString += previousColour + " " + z + "ch, " + replacers[a].val + " " + z + "ch, "; 
+              if(line[z] == regexObjs[a].code){
+                  rtrnString += previousColour + " " + z + "ch, " + regexObjs[a].val + " " + z + "ch, "; 
                   previousChar = line[z];
-                  previousColour = replacers[a].val;
+                  previousColour = regexObjs[a].val;
               }
            }
             }
